ffc7a53c276aa5bf99adb1aad490543958803ff5
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-03-04 15:03:18 -0500

    POST route for DB

--- routes/api/items.js
+++ routes/api/items.js
@@ -8,12 +8,21 @@ const Item = require('../../models/Item');
 // @route GET /api/items
 // @desc get all items
 // @access  Public
-
 router.get('/', (req, res) => {
-  console.log("HIT ROUTE!!!!!!!!!!")
   Item.find()
     .sort({ date: -1 })
     .then(items => res.json(items))
+});
+
+// @route POST /api/items
+// @desc create a post
+// @access  Public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+  newItem.save().then((item) => { res.json(item) });
+
 });
 
 module.exports = router;
