4f07485a9e8753cde6883203672d7eb0a9e45c8a
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-03-05 11:29:23 -0500

    Add Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import AppNavBar from './components/AppNavBar'
 import ShoppingList from './components/ShoppingList'
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavBar />
-      <ShoppingList />
+    <Provider store={store}>
+      <div className="App">
+        <AppNavBar />
+        <ShoppingList />
 
-    </div>
+      </div>
+    </Provider>
   );
 }
 
